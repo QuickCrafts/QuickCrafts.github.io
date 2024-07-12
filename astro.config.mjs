7ea34f09d139bd807be1a5fefc41b9e292c80408
Paula Daniela Guzmán Zabala <pauguzman@Paulas-MacBook-Air.local>
2024-07-11 22:58:53 -0500

    Added Github Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    output: 'static',
+    build: {
+        outDir: 'dist',
+    },
 });
